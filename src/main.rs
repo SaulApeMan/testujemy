c786cda5f815da0d5268aea2612a41cf3152debc
saulapeman <mihaallenko@gmail.com>
2023-09-22 16:03:36 +0200

    Wywalilem komentarze!

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,22 @@ use std::io::{self, BufRead};
 use regex::Regex;
 
 fn main() -> io::Result<()> {
-    // Define a regular expression to match 4-digit numbers
     let re = Regex::new(r"\b\d{4}\b").unwrap();
 
-    // Open the text file
+
     let file = File::open("list_mmr.txt")?;
     let reader = io::BufReader::new(file);
 
-    // Create a vector to store the matching numbers
     let mut four_digit_numbers = Vec::new();
 
-    // Iterate over each line in the file
     for line in reader.lines() {
         let line = line?;
-        // Find all 4-digit numbers in the line and add them to the vector
         for capture in re.captures_iter(&line) {
             let number = &capture[0];
             four_digit_numbers.push(number.to_string());
         }
     }
 
-    // Print the extracted numbers
     for number in &four_digit_numbers {
         println!("{}", number);
     }
